92229de99ebab4c55cbb677b2c6bcf9d9bbf287a
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-03 00:00:44 +0600

    even num and odd number with for loop

---
+++ for.js
@@ -0,0 +1,19 @@
+// for (let num = 0; num<=5; num++){
+//     console.log(num);
+// }
+
+// for (let i =0; i<=21; i++){
+//     console.log(i);
+// }
+
+// even number
+
+// for (let i = 0; i<=50; i=i+2){
+//     console.log(i);
+// }
+
+// odd number
+
+for (let i =1; i<=100; i=i+2){
+    console.log(i);
+}
