b002fde880cc9bcad2ee83efe7efc7ceb313fd77
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-02 23:03:13 +0600

    while loop basic

---
+++ forloop.js
@@ -0,0 +1,10 @@
+// const numbers = [10, 50, 54, 98, 589, 698, 564, 5847, 4788, 854];
+// for(const number of numbers){
+//     console.log(number);
+// };
+
+const fruits = ['apple', 'mango', 'banana', 'tomato', 'malta'];
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log('I eat fruits');
+}
---
+++ while.js
@@ -0,0 +1,12 @@
+let number = 4;
+while (number < 10){
+    console.log('Looping' , number);
+    //number = number + 1;
+    number ++;
+}
+
+let message = 0;
+while (message < 10){
+    console.log('Hello', 10);
+    message ++;
+}
