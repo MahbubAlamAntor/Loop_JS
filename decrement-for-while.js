19ca2b792046955540f85cccc16c031023905787
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-03 09:54:05 +0600

    decrement for and while loop and with sum and i %3

--- decrement-for-while.js
+++ decrement-for-while.js
@@ -6,8 +6,48 @@
 
 //while decrement
 
-let i = 20;
-while (i>=10){
-    console.log(i);
-    i--;
+// let i = 20;
+// while (i>=10){
+//     console.log(i);
+//     i--;
+// }
+
+
+// for (let i = 1; i <=20; i+=2){
+//     console.log(i);
+// }
+
+
+// for (let i =1; i<=20; i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+// }
+
+// for (let i =0; i<=20; i++){
+//     if(i%3===0){
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i =0; i<=20; i++){
+//     if(i%5===0 || i%3===0){
+//         console.log(i)
+//     }
+// }
+
+// for (let i =0; i<=20; i++){
+//     if(i%5===0 && i%3===0){
+//         console.log(i)
+//     }
+// }
+
+let sum = 0;
+for(let i=10; i<=20; i++){
+    if(i%3===0){
+
+        sum = sum+i;
+    }
 }
+console.log('total', sum)
