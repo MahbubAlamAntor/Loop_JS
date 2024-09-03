8991d8d16b318efdc16d43dd7bbe3b6dd7be672e
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-03 10:56:38 +0600

    break contineu with do-while loop

---
+++ break.js
@@ -0,0 +1,15 @@
+// for (let i =10; i<=20; i++){
+//     if(i>15){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+let n =40;
+while(n>20){
+    if(n<30){
+        break;
+    }
+    n--;
+    console.log(n)
+}
